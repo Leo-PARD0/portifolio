f0b4e7f73175e6067eb4154d19eb6ddbc643ff14
Leo-PARD0 <153308202+Leo-PARD0@users.noreply.github.com>
2026-01-28 22:45:04 -0300

    FIX - correção do player iframe, vídeos não são mais cortados.

--- script.js
+++ script.js
@@ -29,3 +29,25 @@ function reveal() {
   }
 }
 window.addEventListener("scroll", reveal);
+
+// ===== EFEITO TILT NOS CARDS =====
+document.querySelectorAll(".tilt").forEach(card => {
+  card.addEventListener("mousemove", e => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const midX = rect.width / 2;
+    const midY = rect.height / 2;
+
+    const rotateY = ((x - midX) / midX) * 6;
+    const rotateX = ((midY - y) / midY) * 6;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.03)`;
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "rotateX(0) rotateY(0) scale(1)";
+  });
+});
+
